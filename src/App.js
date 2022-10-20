ac5ab6e7ac8dcb4f5f8aba25d4e702f79340c9de
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-20 08:24:50 +0500

    added new section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import FeaturesBanner from "layouts/FeaturesBanner";
 import Navbar from "layouts/Navbar";
 import React from "react";
 import Hero from "./layouts/Hero";
@@ -8,6 +9,10 @@ function App() {
       <div className="relative">
         <Navbar />
         <Hero />
+      </div>
+
+      <div className="mb-20">
+        <FeaturesBanner />
       </div>
     </div>
   );
