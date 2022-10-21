2f58a9d590bd3e4795101773d40503afdb40e95e
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2022-10-21 12:21:48 +0500

    changes made

--- src/layouts/Team.js
+++ src/layouts/Team.js
@@ -7,7 +7,8 @@ function Team() {
   return (
     <div className="py-16 sm:py-24 xl:py-32 relative z-10">
       <div className="container">
-        <div className="max-w-[1018px] w-full mx-auto z-10 relative">
+        {/* max-w-[1018px] */}
+        <div className=" w-full mx-auto z-10 relative">
           <img
             src="images/quote.png"
             className="absolute bottom-[80%] md:bottom-[50%] right-[70%] -z-20 opacity-20 w-[28%] md:w-[20%] xl:w-[30%] rotate-180 pointer-events-none select-none"
@@ -38,6 +39,7 @@ function Team() {
                 return `<div  class=${className}></div>`;
               },
             }}
+            className="team-slider-wrapper"
           >
             <SwiperSlide>
               <TeamCard />
@@ -61,7 +63,7 @@ function Team() {
         </div>
       </div>
 
-      <div className="sm:h-[60vh] w-[76%] sm:w-full -z-20 team-slider absolute top-[92%] sm:top-1/2 left-1/2 sm:left-0 -translate-x-1/2 sm:translate-x-0 sm:px-[4%] sm:-translate-y-1/2 sm:grid sm:grid-cols-[3px_3px] sm:justify-between sm:gap-6 flex justify-center items-center space-x-4 sm:space-x-0"></div>
+      <div className="sm:h-[80%] sm:max-h-[400px] w-[76%] sm:w-full -z-20 team-slider absolute top-[92%] sm:top-1/2 left-1/2 sm:left-0 -translate-x-1/2 sm:translate-x-0 sm:px-[4%] sm:-translate-y-1/2 sm:grid sm:grid-cols-[3px_3px] sm:justify-between sm:gap-6 flex justify-center items-center space-x-4 sm:space-x-0"></div>
     </div>
   );
 }
